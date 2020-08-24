ead28236093c2532a574964ff6e356da56d8509a
Tomislav Valentić <tvalentic@combis.hr>
2020-08-24 14:55:20 +0200

    Updates

---
+++ precache-manifest.0173aec2b0c0a5dc0df6b46aae708160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebb3e2cf73afbfb840f77605512dfc4",
+    "url": "/TVaTestReact/index.html"
+  },
+  {
+    "revision": "ed91a2cf38301e9c15c6",
+    "url": "/TVaTestReact/static/css/main.581b7527.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/TVaTestReact/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TVaTestReact/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed91a2cf38301e9c15c6",
+    "url": "/TVaTestReact/static/js/main.0e75370a.chunk.js"
+  },
+  {
+    "revision": "9d8d97a7924745afc8a0",
+    "url": "/TVaTestReact/static/js/runtime-main.4e23d23f.js"
+  }
+]);
