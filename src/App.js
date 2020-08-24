f6b22d442bad7841b21165554246243fbacb0764
Tomislav Valentić <tvalentic@combis.hr>
2020-08-24 14:04:36 +0200

    Test deploy TVa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import "./App.css";
+import SearchBox from "./components/serch-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+
+    //Ovaj di koda ne treba kao koristimo sintaksu: handleChange = (e) => {}
+    //Ali ako koristimo sintaksu bez strelice onda ova linija ispod treba: handleChange(e){}
+    /* this.handleChange = this.handleChange.bind(this); */
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  printState = () => {
+    console.log("PrintState ", this.state);
+  };
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value }, this.printState);
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
