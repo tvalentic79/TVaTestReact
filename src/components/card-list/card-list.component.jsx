316021d7474ab636af85d6db4773e8abe25ba052
Tomislav Valentić <tvalentic@combis.hr>
2020-08-25 13:04:09 +0200

    New deploy

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,12 +4,12 @@ import "./card-list.stayles.css";
 /* import Card from 'react-bootstrap/Card'; */
 import CardDeck from "react-bootstrap/CardDeck";
 
+/* <div className="card-list"> */
+/*  <Card key={monster.id} monster={monster} /> */
+
 const CardList = (props) => {
   console.log(props);
   return (
-    /* <div className="card-list"> */
-    /*  <Card key={monster.id} monster={monster} /> */
-
     <CardDeck className="card-list">
       {props.monsters.map((monster) => (
         <MyCard key={monster.id} monster={monster} />
--- src/components/card/my-card.component.jsx
+++ src/components/card/my-card.component.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import "./card.styles.css";
 import Card from "react-bootstrap/Card";
 
-const MyCard = (props) => {
-  return (
-    /* <div className="card card-container">
+/* <div className="card card-container">
       <img
         alt="monster"
         src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
@@ -12,20 +10,23 @@ const MyCard = (props) => {
       <h2>{props.monster.name}</h2>
       <p> {props.monster.email}</p>
     </div> */
-<div className="my-card">
-    <Card>
-      <Card.Img
-        variant="top"
-        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
-      />
-      <Card.Body>
-        <Card.Title>{props.monster.name}</Card.Title>
-        <Card.Text>This content is a little bit longer.</Card.Text>
-      </Card.Body>
-      <Card.Footer>
-        <small className="text-muted">{props.monster.email}</small>
-      </Card.Footer>
-    </Card>
+
+const MyCard = (props) => {
+  return (
+    <div className="my-card">
+      <Card>
+        <Card.Img
+          variant="top"
+          src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        />
+        <Card.Body>
+          <Card.Title>{props.monster.name}</Card.Title>
+          <Card.Text>This content is a little bit longer.</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">{props.monster.email}</small>
+        </Card.Footer>
+      </Card>
     </div>
   );
 };
